2ed9c036b53b47a1aa5f2f4a31c4831735de4e5e
Gabriel Musskopf <gabrielgmusskopf@gmail.com>
2024-01-24 22:08:47 -0300

    feat: Ask if want to use previous branch as target

--- main.go
+++ main.go
@@ -6,10 +6,13 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"net/url"
 	"os"
 	"os/exec"
 	"strings"
 )
+
+var allYes bool
 
 func openBrowser(url string) {
 	cmd := exec.Command("xdg-open", url)
@@ -31,10 +34,50 @@ func init() {
 		fmt.Fprintf(os.Stderr, "Usage: %s\n", os.Args[0])
 		flag.PrintDefaults()
 	}
+
+}
+
+func gitCurrentBranch() (string, error) {
+	output, err := exec.Command("git", "branch", "--show-current").Output()
+	if err != nil {
+		return "", err
+	}
+	return string(bytes.TrimSpace(output)), nil
+}
+
+func gitPreviousBranch() (string, error) {
+	output, err := exec.Command("git", "rev-parse", "--symbolic-full-name", "--abbrev-ref=loose", "@{-1}").Output()
+	if err != nil {
+		return "", err
+	}
+	return string(bytes.TrimSpace(output)), nil
+}
+
+func gitPush(branch string) (string, error) {
+	var buff bytes.Buffer
+	pushCmd := exec.Command("git", "push", "-u", "origin", branch, "--progress")
+	pushCmd.Stderr = io.MultiWriter(&buff, os.Stdout)
+
+	if err := pushCmd.Run(); err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(buff.String()), nil
+}
+
+func getAnswerBool(question string) bool {
+	var response string
+	if allYes {
+		response = "y"
+	} else {
+		fmt.Print(question)
+		fmt.Scanln(&response)
+	}
+
+    return response == "y"
 }
 
 func main() {
-	allYes := flag.Bool("y", false, "accept all")
+	flag.BoolVar(&allYes, "y", false, "Accept all. This means open a Merge Request and use previous branch as target")
 	flag.Parse()
 
 	if !isGitRepository() {
@@ -42,24 +85,26 @@ func main() {
 		return
 	}
 
-	currentBranchOutput, err := exec.Command("git", "branch", "--show-current").Output()
+	previousBranch, err := gitPreviousBranch()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "ERROR: Could not get previous branch", err)
+		return
+	}
+
+	currentBranch, err := gitCurrentBranch()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "ERROR: Could not get current branch", err)
 		return
 	}
 
-	var buff bytes.Buffer
-	branch := string(bytes.TrimSpace(currentBranchOutput))
-	pushCmd := exec.Command("git", "push", "-u", "origin", branch, "--progress")
-	pushCmd.Stderr = io.MultiWriter(&buff, os.Stdout)
-
-	if err := pushCmd.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: Could not run command: %v", err)
+	push, err := gitPush(currentBranch)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: Could not push: %v", err)
 		return
 	}
-	output := strings.TrimSpace(buff.String())
-	scanner := bufio.NewScanner(strings.NewReader(output))
 
+
+	scanner := bufio.NewScanner(strings.NewReader(push))
 	for scanner.Scan() {
 		line := scanner.Text()
 		if strings.Contains(line, "Everything up-to-date") {
@@ -67,25 +112,22 @@ func main() {
 		}
 		if strings.Contains(line, "To create a merge request for") || strings.Contains(line, "View merge request for") {
 			if scanner.Scan() {
-				url := strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "remote:"))
-				var response string
-				if *allYes {
-					response = "s"
-				} else {
-					fmt.Printf("\nDeseja abrir o Merge Request para %s? (s/n): ", branch)
-					fmt.Scanln(&response)
-				}
-				if strings.ToLower(response) == "s" {
-					openBrowser(url)
+				u := strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "remote:"))
+                openMerge := getAnswerBool(fmt.Sprintf("\nDeseja abrir o Merge Request para %s? (y/n): ", currentBranch))
+				if openMerge {
+                    usePreviousAsTarget := getAnswerBool(fmt.Sprintf("Deseja usar a última branch (%s) como target? (y/n): ", previousBranch))
+					if usePreviousAsTarget {
+						u += "&merge_request" + url.QueryEscape("[target_branch]") + "=" + url.QueryEscape(previousBranch)
+					}
+					openBrowser(u)
 				}
 				break
 			}
 		}
 	}
 
-	//TODO: Perguntar se deseja usar target branch
-
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "ERROR: ", err)
 	}
+
 }
